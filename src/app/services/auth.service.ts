db8d0240deae4568b080e2f1808b9f971b9272d3
CGonzalez97 <cgl76490@gmail.com>
2021-01-19 20:53:32 +0100

    Avance

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -10,6 +10,8 @@ import {googAuthUser} from '../models/user.interface';
 })
 export class AuthService {
 
+  
+
   constructor(
     public afs: AngularFirestore,   // BASE DE DATOS EN Firestore
     public afAuth: AngularFireAuth, // SERVICIO DE AUTH en Firebase
@@ -21,9 +23,16 @@ export class AuthService {
       const provider = new firebase.auth.GoogleAuthProvider();
       return this.afAuth.signInWithPopup(provider).then((result) => {
         console.log(result);
-        const perfilUsuarioLogueado = result.additionalUserInfo.profile;
-        //const {email="", family_name: apellidos = "", given_name: nombre = "", picture: foto = "" =  perfilUsuarioLogueado || {};
-        this.afs.collection('users').add(perfilUsuarioLogueado);
+        let perfilUsuarioLogueado:any;
+        perfilUsuarioLogueado = result.additionalUserInfo.profile;
+        //const perfilUsuarioLogueado = result.additionalUserInfo.profile;
+        const {email="", family_name: apellidos = "", given_name: nombre = "", picture: foto = "" }=  perfilUsuarioLogueado || {};
+        
+        let consulta:any = (this.afs.collection('users'), ref => ref.where('email','==',this.afAuth.currentUser));
+        if(consulta!=undefined && consulta!=null && consulta.lenght>0 && perfilUsuarioLogueado.email == consulta.email){
+          this.afs.collection('users').add(perfilUsuarioLogueado);
+        }
+        
 
       }).catch((error) => {
         window.alert(error)
